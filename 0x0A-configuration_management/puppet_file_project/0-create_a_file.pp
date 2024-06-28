b8e4d67414d1957728d21db6c23e9a6ce4bee989
Mikonimo <mikonani413@gmail.com>
2024-06-29 02:19:50 +0300

    task_s

--- 0x0A-configuration_management/puppet_file_project/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# This Puppet manifest creates a file at /tmp/school with specific permissions, ownership, and content.
-file { "/tmp/school":
-  ensure  => file,
-  content => "I love Puppet",
-  mode    => "0744",
-  owner   => "www-data",
-  group   => "www-data",
-}
-

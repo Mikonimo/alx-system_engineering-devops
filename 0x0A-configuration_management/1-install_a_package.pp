d4e6b3bbc74f45ee4b3534134db478cf7768ffc6
Mikonimo <mikonani413@gmail.com>
2024-07-04 10:25:41 +0300

    installation

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Using Puppet, install flask from pip3
-package { 'flask':
+# This Puppet manifest installs Flask version 2.1.0 using pip3.
+package { ['Flask', 'Werkzeug']:
   ensure   => '2.1.0',
   provider => 'pip3',
 }

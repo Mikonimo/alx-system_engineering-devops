9124e088810bbbc8515a4b09963e2acbf73902c1
Mikonimo <mikonani413@gmail.com>
2024-07-29 23:39:08 +0300

    Task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""This module gathers data from an API"""
+import requests
+import sys
+
+
+def fetch_employee_data(employee_id):
+    # Base URL of the API
+    base_url = 'https://jsonplaceholder.typicode.com'
+
+    # Fetch employee data
+    user_url = f'{base_url}/users/{employee_id}'
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+
+    # Fetch todos for the employee
+    todos_url = f'{base_url}/todos?userId={employee_id}'
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
+
+    # Extract employee name
+    employee_name = user_data['name']
+
+    # Extract and count tasks
+    total_tasks = len(todos_data)
+    done_tasks = [task for task in todos_data if task['completed']]
+    number_of_done_tasks = len(done_tasks)
+
+    # Print the required output
+    print(f'Employee {employee_name} is done with tasks(\
+        {number_of_done_tasks}/{total_tasks}):')
+    for task in done_tasks:
+        print(f'\t {task["title"]}')
+
+
+if __name__ == '__main__':
+    # Ensure an employee ID is provided as a command-line argument
+    if len(sys.argv) != 2:
+        print('Usage: python3 script_name.py EMPLOYEE_ID')
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print('EMPLOYEE_ID must be an integer.')
+        sys.exit(1)
+
+    fetch_employee_data(employee_id)
---
+++ 0x15-api/README.md
@@ -0,0 +1,3 @@
+## API ##
+An Application Program Interface is a set of routines, protocols, and tools for building software applications.
+Basically, an API specifies how software components should interact.
